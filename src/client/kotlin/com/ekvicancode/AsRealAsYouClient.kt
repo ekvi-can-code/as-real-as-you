e5ec7b3101e60f24ce2ca59852ce53f5a9f07b39
Ekvi <kornevila41@gmail.com>
2026-02-20 16:49:16 +0900

    ARAY

---
+++ src/client/kotlin/com/ekvicancode/AsRealAsYouClient.kt
@@ -0,0 +1,9 @@
+package com.ekvicancode
+
+import net.fabricmc.api.ClientModInitializer
+
+object AsRealAsYouClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
